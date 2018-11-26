db19d767f6ccef5cf94f571412ba9e3d5ca9bdd7
Christoffer Lilja <christoffer.lilja@syb.sh>
2018-11-26 16:08:56 +0100

    ...

--- web.py
+++ web.py
@@ -1,10 +1,11 @@
+''' Doc string... '''
 from flask import Flask
  
-app = Flask(__name__)
+APP = Flask(__name__)
  
-@app.route('/')
+@APP.route('/')
 def index():
     return "Hello World!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=80)
+    APP.run(host='0.0.0.0',port=80)
